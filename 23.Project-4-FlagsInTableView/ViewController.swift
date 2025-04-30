dd5060f03a4809d99b3b231b78a573e15ce9c730
Valentin <latronixo@gmail.com>
2025-04-30 16:50:13 +0300

    Initial Commit

---
+++ 23.Project-4-FlagsInTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  23.Project-4-FlagsInTableView
+//
+//  Created by Валентин Картошкин on 30.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
